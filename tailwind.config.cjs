100e383811281972d40f4263f7129364544c2540
Vyast <me@vyast.dev>
2023-03-29 17:14:04 -0600

    initial design

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        industry: ["Industry", ...fontFamily.sans],
+      },
+      backgroundImage: {
+        topography: "url('/topography.svg')",
+      },
+      screens: {
+        xs: "384px",
+      },
+    },
+  },
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  plugins: [],
+};

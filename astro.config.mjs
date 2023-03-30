0f54bd2d6ae9a69f50224601b7dae950abaeae6b
Vyast <me@vyast.dev>
2023-03-30 03:20:12 -0600

    try different method of setting up analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
+//@ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
   site: "https://vyast.dev",
-  output: "static",
-  adapter: vercel({
-    analytics: true,
-  }),
 });

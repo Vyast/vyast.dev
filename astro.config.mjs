bcddad50bc60c3474069c169b10687469a10aa00
Vyast <me@vyast.dev>
2023-04-25 04:32:38 -0600

    use @astrojs/vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+import vercel from "@astrojs/vercel/static";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
   site: "https://vyast.dev",
+  adapter: vercel({
+    analytics: true,
+  }),
 });
